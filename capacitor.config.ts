c5bc076d786ad13eff846aa1db4cacffb7ed548a
Abdul <NMMART07@GMAIL.COM>
2026-04-02 17:41:47 +0530

    Ignoring warnings for build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nmmart.pos',
+  appName: 'nm mart',
+  webDir: 'build'
+};
+
+export default config;
